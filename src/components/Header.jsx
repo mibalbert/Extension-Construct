3832c0019e5f24ed1c1cbb585de571fcc7977d97
Alberto Paros <mib.albert@gmail.com>
2024-03-09 17:57:27 +0200

    finished the landign page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,82 +45,84 @@ const Header = () => {
       }`}
     >
       <nav>
-        <Container className="relative z-50 flex justify-between py-3">
+        <Container className="relative z-50 flex justify-between py-3 ">
           {/* Logo */}
           <div className="relative z-10 flex items-center gap-16">
             <Logo />
           </div>
           {/* NavLinks */}
-          <div className="hidden lg:flex lg:gap-10 items-center">
-            <NavLinks />
-          </div>
-          {/* Buttons */}
-          <div className="flex items-center gap-6">
-            <Button href="#" variant="outline" className="hidden lg:block">
-              Get Advice
-            </Button>
-            <Button href="#" className="hidden lg:block">
-              Hire me
-            </Button>
-            {/* Mobile NavLinks */}
-            <Popover className="lg:hidden">
-              {({ open }) => (
-                <>
-                  <Popover.Button
-                    className="relative z-10 -m-2 inline-flex items-center rounded-lg stroke-gray-900 p-2 hover:bg-gray-200/50 hover:stroke-gray-600 active:stroke-gray-900 [&:not(:focus-visible)]:focus:outline-none outline-none"
-                    aria-label="Toggle site navigation"
-                  >
-                    {({ open }) =>
-                      open ? (
-                        <IoIosArrowUp className="text-2xl" />
-                      ) : (
-                        <TbMenu2 className="text-2xl" />
-                      )
-                    }
-                  </Popover.Button>
-                  <AnimatePresence initial={false}>
-                    {open && (
-                      <>
-                        <Popover.Overlay
-                          static
-                          as={motion.div}
-                          initial={{ opacity: 0 }}
-                          animate={{ opacity: 1 }}
-                          exit={{ opacity: 0 }}
-                          className="fixed inset-0 z-0 bg-gray-300/60 backdrop-blur"
-                        />
-                        <Popover.Panel
-                          static
-                          as={motion.div}
-                          initial={{ opacity: 0, y: -32 }}
-                          animate={{ opacity: 1, y: 0 }}
-                          exit={{
-                            opacity: 0,
-                            y: -32,
-                            transition: { duration: 0.2 },
-                          }}
-                          className="absolute inset-x-0 top-0 z-0 origin-top rounded-b-2xl bg-gray-50 px-6 pb-6 pt-32 shadow-2xl shadow-gray-900/20"
-                        >
-                          <div className="space-y-4">
-                            {navData.map(({ _id, title, href }) => (
-                              <MobileNavLink href={href} key={_id}>
-                                {title}
-                              </MobileNavLink>
-                            ))}
-                          </div>
-                          <div className="mt-8 flex flex-col gap-4">
-                            <Button href="#" variant="outline">
-                              Get Advice
-                            </Button>
-                            <Button href="#">Hire me</Button>
-                          </div>
-                        </Popover.Panel>
-                      </>
-                    )}
-                  </AnimatePresence>
-                </>
-              )}
-            </Popover>
+          <div className="">
+            <div className="hidden lg:flex lg:gap-5 items-center">
+              <NavLinks />
+              <Button href="#" variant="outline" className="">
+                Get Advice
+              </Button>
+              <Button href="#" className="">
+                Hire me
+              </Button>
+            </div>
+            {/* Buttons */}
+            <div className="flex items-center gap-6">
+              {/* Mobile NavLinks */}
+              <Popover className="lg:hidden">
+                {({ open }) => (
+                  <>
+                    <Popover.Button
+                      className="relative z-10 -m-2 inline-flex items-center rounded-lg stroke-gray-900 p-2 hover:bg-gray-200/50 hover:stroke-gray-600 active:stroke-gray-900 [&:not(:focus-visible)]:focus:outline-none outline-none"
+                      aria-label="Toggle site navigation"
+                    >
+                      {({ open }) =>
+                        open ? (
+                          <IoIosArrowUp className="text-2xl" />
+                        ) : (
+                          <TbMenu2 className="text-2xl" />
+                        )
+                      }
+                    </Popover.Button>
+                    <AnimatePresence initial={false}>
+                      {open && (
+                        <>
+                          <Popover.Overlay
+                            static
+                            as={motion.div}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            className="fixed inset-0 z-0 bg-gray-300/60 backdrop-blur"
+                          />
+                          <Popover.Panel
+                            static
+                            as={motion.div}
+                            initial={{ opacity: 0, y: -32 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{
+                              opacity: 0,
+                              y: -32,
+                              transition: { duration: 0.2 },
+                            }}
+                            className="absolute inset-x-0 top-0 z-0 origin-top rounded-b-2xl bg-gray-50 px-6 pb-6 pt-32 shadow-2xl shadow-gray-900/20"
+                          >
+                            <div className="space-y-4">
+                              {navData.map(({ _id, title, href }) => (
+                                <MobileNavLink href={href} key={_id}>
+                                  {title}
+                                </MobileNavLink>
+                              ))}
+                            </div>
+                            <div className="mt-8 flex flex-col gap-4">
+                              <Button href="#" variant="outline">
+                                Get Advice
+                              </Button>
+                              <Button href="#">Hire me</Button>
+                            </div>
+                          </Popover.Panel>
+                        </>
+                      )}
+                    </AnimatePresence>
+                  </>
+                )}
+              </Popover>
+            </div>
           </div>
         </Container>
       </nav>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,46 +9,66 @@ import ExtraLogos from "./ExtraLogos";
 import BackgroundDesign from "./BackgroundDesign";
 import PhoneFrame from "./PhoneFrame";
 import AppFeature from "./AppFeature";
+import Link from "next/link";
+import { PhoneCallIcon } from "lucide-react";
+import { PhoneIcon } from "lucide-react";
+import { ArrowRightIcon } from "lucide-react";
+import { Calculator } from "lucide-react";
 
 const Hero = () => {
   return (
     <section
       id="home"
-      className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36 bg-[url('/hero-img.webp')] bg-cover bg-center"
+      className="relative overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36 bg-[url('/hero-img.webp')] bg-cover   bg-right-bottom "
     >
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-gray-900/50 to-gray-700/40 z-10  backdrop-blur-sm  supports-[backdrop-blur]" />
       <Container>
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20 ">
           {/* Right side */}
-          <div className="relative z-10 mx-auto  lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
+          <div className="relative z-20 mx-auto  lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <Title
               title="Extension Construct"
-              className="text-7xl w-full lg:whitespace-nowrap font-bold text-black font-serif "
+              className="text-7xl lg:text-8xl w-full lg:whitespace-nowrap font-bold text-white  "
             />
             <Title
               title="Invest at the perfect time."
-              className="text-4xl max-w-2xl"
+              className="text-2xl lg:text-4xl max-w-2xl text-gray-100 font-serif"
             />
-            <p className="mt-6 text-lg text-gray-600">
+            <p className="mt-6 text-md lg:text-lg text-gray-200">
               By leveraging insights from our network of industry insiders, you
               will know exactly when to buy to maximize profit, and exactly when
               to sell to avoid painful losses.
             </p>
-            <div className="mt-8 flex flex-wrap items-center gap-x-6 gap-4">
-              <Image className="w-32 h-auto" src={playStore} alt="playImg" />
+            <div className="mt-8 space-y-6 gap-4">
+              {/* <Image className="w-32 h-auto" src={playStore} alt="playImg" />
               <Button variant="outline" href="https://youtu.be/vviFia-Stqk">
                 <BsPlayCircle className="text-xl" />
                 <span className="ml-2.5">Watch the video</span>
-              </Button>
+              </Button> */}
+              <Link
+                href={"/contact"}
+                className="bg-white px-3 py-1.5 rounded-lg text-2xl w-full max-w-xs flex items-center gap-2 "
+              >
+                <ArrowRightIcon className="w-6 h-6" />
+                <span className="font-serif">Contact</span>
+              </Link>
+              <Link
+                href="/preturi"
+                className="bg-white px-3 py-1.5 rounded-lg text-2xl flex items-center w-full max-w-xs gap-2 "
+              >
+                <Calculator className="w-6 h-6" />
+                <span className="font-serif">Calculator</span>
+              </Link>
             </div>
           </div>
           {/* Left side */}
           <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
-            <BackgroundDesign className="absolute left-1/4 top-4 h-[1026px] w-[1026px] -translate-x-1/4 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
-            {/* <div className="-mx-4 h-[448px] px-9  [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32"> */}
-            {/* <PhoneFrame className="max-w-[366px] mx-auto">
+            <BackgroundDesign className="absolute left-1/4 top-4 h-[1026px] z-10 w-[1026px] -translate-x-1/4 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
+            {/* <div className="-mx-4 h-[448px] px-9  [mask-image:linear-gradient(to_bottom,white_60%,white)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
+            <PhoneFrame className="max-w-[366px] mx-auto">
               <AppFeature />
-            </PhoneFrame> */}
-            {/* </div> */}
+            </PhoneFrame>
+            </div> */}
           </div>
         </div>
       </Container>
