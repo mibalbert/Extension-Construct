d288f9399ea8ed6ee02bc186de7f7f51f5cd793f
Alberto Paros <mib.albert@gmail.com>
2024-03-09 17:20:09 +0200

    close to finish the pricing calculator

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36 bg-[url('/hero-img.webp')] bg-cover"
+      className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36 bg-[url('/hero-img.webp')] bg-cover bg-center"
     >
       <Container>
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20 ">
