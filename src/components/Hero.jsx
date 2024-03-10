8aac03361a4bb6cb0043be43894e8166ef328d90
Alberto Paros <mib.albert@gmail.com>
2024-03-10 16:00:59 +0200

    remade the landing-hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
             <div className="grid  grid-cols-1 lg:grid-cols-3 gap-5 h-full px-10 lg:px-0">
               <Link
                 href={"/contact"}
-                className="flex items-center  border relative bg-white rounded-lg w-full h-full justify-center hover:scale-105 transition-all duration-200 bg-grid-small-black/[0.2] "
+                className="flex items-center  border relative bg-white rounded-lg w-full h-full justify-center hover:scale-105 transition-all duration-100 bg-grid-small-black/[0.2] "
               >
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
                 <Phone className="h-4 w-4 mr-2" />
@@ -51,7 +51,7 @@ const Hero = () => {
               </Link>
               <Link
                 href={"/preturi"}
-                className="flex items-center border bg-white  rounded-lg w-full h-full justify-center hover:scale-105 transition-all duration-200 bg-dot-black/[0.2] relative"
+                className="flex items-center border bg-white  rounded-lg w-full h-full justify-center hover:scale-105 transition-all duration-100 bg-dot-black/[0.2] relative"
               >
                 {" "}
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
@@ -60,7 +60,7 @@ const Hero = () => {
               </Link>
               <Link
                 href={"/blog"}
-                className="flex items-center border bg-white rounded-lg  w-full h-full justify-center hover:scale-105 transition-all duration-200 bg-grid-black/[0.1]  relative"
+                className="flex items-center border bg-white rounded-lg  w-full h-full justify-center hover:scale-105 transition-all duration-100 bg-grid-black/[0.1]  relative"
               >
                 {" "}
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
